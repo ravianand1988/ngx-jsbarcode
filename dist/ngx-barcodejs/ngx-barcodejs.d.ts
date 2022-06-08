8b1db97cf2f32f9978b4256661d2eab5788df553
Ravi Anand Kumar <ravianand1988@live.com>
2022-06-08 20:25:22 +0200

    Publish dist on git

---
+++ dist/ngx-barcodejs/ngx-barcodejs.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public-api';
+//# sourceMappingURL=ngx-barcodejs.d.ts.map
---
+++ dist/ngx-barcodejs/public-api.d.ts
@@ -0,0 +1,3 @@
+export * from './lib/ngx-barcode-js/ngx-barcode/ngx-barcode.component';
+export * from './lib/ngx-barcode-js/ngx-barcode-js.module';
+//# sourceMappingURL=public-api.d.ts.map
